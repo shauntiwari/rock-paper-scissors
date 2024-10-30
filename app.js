cfb57dd437efb2c3b32c6eac3f88c11b020fc12c
Shaun Tiwari <67908289+shauntiwari@users.noreply.github.com>
2024-10-30 12:06:25 -0400

    Completed rock paper scissors game

--- app.js
+++ app.js
@@ -1,31 +1,91 @@
 
-function getComputerChoice(){
+function getComputerChoice() {
     let random = Math.ceil(Math.random() * 3);
     
     if (random === 1) {
-        return 'rock';
+        return 'Rock';
     }
     else if (random === 2) {
-        return 'paper';
+        return 'Paper';
     }
     else {
-        return 'scissors';
+        return 'Scissors';
     }
 }
 
 function getHumanChoice() {
-    const input = prompt('Hello, choose r for rock, p for paper, or s for scissors!');
-    if (input === 'r' || input === 'R'){
-        return 'rock';
+    const input = prompt('Hello, choose Rock, Paper, or Scissors!');
+    const choice = input.toLowerCase();
+    if (choice === 'rock') {
+        return 'Rock';
     }
-    else if (input === 'p' || input === 'P'){
-        return 'paper';
+    else if (choice === 'paper') {
+        return 'Paper';
     }
-    else if (input === 's' || input === 'S'){
-        return 'scissors';
+    else if (choice === 'scissors') {
+        return 'Scissors';
+    }
+    else {
+        alert('Invalid choice!');
+        return getHumanChoice();
     }
 }
 
-console.log('You chose' + ' ' + getHumanChoice());
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    for (i = 1; i <= 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
 
-console.log('Computer chose' + ' ' + getComputerChoice());
+    console.log('Final Score | Computer: ' + computerScore + ' | You: ' + humanScore);
+    
+    if (humanScore > computerScore) {
+        console.log("You win! Computer loses!");
+    }
+    else if (computerScore > humanScore) {
+        console.log("Computer wins! You lose!")
+    }
+    else {
+        console.log("It's a tied game!")
+    }
+
+    function playRound(humanChoice, computerChoice) {
+        console.log('You chose' + ' ' + humanChoice + ', Computer chose ' + computerChoice);
+        
+        if (humanChoice === computerChoice) {
+            console.log("Tie game!");
+        }
+        else if (humanChoice === 'Rock' && computerChoice === 'Paper') {
+            console.log('Computer wins!')
+            computerScore++;
+        }
+        else if (humanChoice === 'Rock' && computerChoice === 'Scissors') {
+            console.log('You win!')
+            humanScore++;
+        }
+        else if (humanChoice === 'Scissors' && computerChoice === 'Rock') {
+            console.log('Computer wins!')
+            computerScore++;
+        }
+        else if (humanChoice === 'Scissors' && computerChoice === 'Paper') {
+            console.log('You win!')
+            humanScore++;
+        }
+        else if (humanChoice === 'Paper' && computerChoice === 'Rock') {
+            console.log('You win!')
+            humanScore++;
+        }
+        else if (humanChoice === 'Paper' && computerChoice === 'Scissors') {
+            console.log('Computer wins!')
+            computerScore++;
+        }
+
+        console.log('Scoreboard | Computer: ' + computerScore + ' | You: ' + humanScore);
+    }    
+}
+
+playGame();
